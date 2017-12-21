bce2fe4b482f884ec840378a5243b1d0138e155c
Menuka Weerasooriya <mnw@sl.tiqri.com>
2017-12-21 16:05:19 +0530

    Add project files.

---
+++ MobileBillingTests/MobileBillingTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MobileBillingTests
+{
+    [TestFixture]
+    public class MobileBillingTest
+    {
+        [Test]
+        public void OnSort_WithOnlyNegativeNumberList_ShouldReturnAccendinlySortedNumberList()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
