db512e3b8cfc20fb95c065ddddd5f8033e3af9aa
Menuka Weerasooriya <mnw@tiqri.com>
2018-01-02 17:11:41 +0530

    removing unnecessary imports...

--- MobileBilling/Program.cs
+++ MobileBilling/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MobileBilling
 {
@@ -10,9 +6,7 @@ namespace MobileBilling
     {
         static void Main(string[] args)
         {
-            double _peakHoursLocalPerSecondCharge = 4;
-           _peakHoursLocalPerSecondCharge = Math.Round(_peakHoursLocalPerSecondCharge, 2);
-            Console.WriteLine(_peakHoursLocalPerSecondCharge);
+
         }
     }
 }
